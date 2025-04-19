485a79a6e791b0eeca0248070db16d0b9d76306d
Michael Meffie <mmeffie@sinenomine.net>
2025-04-18 22:29:25 -0400

    Fix syntax warning in get_cache_size
    Use a raw string for the regex in the get_cache_size to fix the syntax
    warning:
    
      ../OpenAFSLibrary/keywords/cache.py:34: SyntaxWarning: invalid escape sequence '\d'
         size = re.findall('\d+', s) # find the decimal values in the string

--- OpenAFSLibrary/keywords/cache.py
+++ OpenAFSLibrary/keywords/cache.py
@@ -31,5 +31,5 @@ class _CacheKeywords(object):
 
         Outputs AFS cache size as the number of 1K blocks."""
         s = fs("getcacheparms") # get output string
-        size = re.findall('\d+', s) # find the decimal values in the string
+        size = re.findall(r'\d+', s) # find the decimal values in the string
         return int(size[1]) # output integer for number of 1K blocks
